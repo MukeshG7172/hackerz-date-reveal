797253dd7137962c157d50e91bee8ed221f7f0c9
MukeshG7172 <mukeshrjpm1@gmail.com>
2024-12-22 17:28:46 +0530

    Removed the api fetch for questions

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,37 @@
-// src/app/page.js
-import prisma from '@/lib/prisma';
 import QuizStartPage from '@/components/QuizStartPage'; 
 
-async function getQuestions() {
-  try {
-    const questions = await prisma.question.findMany({
-      orderBy: { id: 'asc' }
-    });
-    return questions;
-  } catch (error) {
-    console.error('Error fetching questions:', error);
-    return [];
-  }
-}
 
 export default async function Page() {
-  const questions = await getQuestions();
+  const questions = [
+    {
+      id: 1,
+      question: "What is the theme of Hackerz 2025?",
+      option1: "White Hat Hackers",
+      option2: "Black Hat Hackers",
+      option3: "Cybersecurity",
+      option4: "Communication",
+      answer: "White Hat Hackers" 
+    },
+    {
+      id: 2,
+      question: "White Hat Hackers are know for?",
+      option1: "Stealing data",
+      option2: "Ethical Hacking",
+      option3: "Non Ethical Hacking",
+      option4: "Creating Viruses",
+      answer: "Ethical Hacking"
+    },
+    {
+      id: 3,
+      question: "What is the FlagShip Event of Hackerz 2025?",
+      option1: "Code-a-thon",
+      option2: "Idea-a-thon(Seeds Start)",
+      option3: "Competitive Programming",
+      option4: "AI Challenge",
+      answer: "Idea-a-thon(Seeds Start)"
+    }
+  ];
+  
   
   return <QuizStartPage initialQuestions={questions} />;
 }
