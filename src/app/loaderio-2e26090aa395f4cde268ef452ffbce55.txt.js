d5f65b789abc9f8cd89b311fb58072b45642b069
MukeshG7172 <mukeshrjpm1@gmail.com>
2024-12-22 17:43:32 +0530

    Loader.io file

---
+++ src/app/loaderio-2e26090aa395f4cde268ef452ffbce55.txt.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+    res.status(200).send('loaderio-2e26090aa395f4cde268ef452ffbce55');
+  }
+  
+  export const config = {
+    api: {
+      bodyParser: false,
+    },
+  };
+  
